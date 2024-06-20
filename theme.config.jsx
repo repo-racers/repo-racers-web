ab24ca52b73747b1befc2ef0d8ed546d671ebc09
Kevin Ryan <kevinryan1973@gmail.com>
2024-06-20 10:55:29 +0000

    Remove edit and feedback links

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,14 @@
+import { Component } from "react";
+
 export default {
-    logo: <span>Repo Racers</span>,
-    project: {
-      link: 'https://github.com/repo-racers/repo-racers-web'
-    }
-  }
-  
+  logo: <span>Repo Racers</span>,
+  project: {
+    link: 'https://github.com/repo-racers/repo-racers-web',
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+}
