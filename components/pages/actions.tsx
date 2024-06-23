6ff22909fef5c6526f8fce447f4a4d1e3eb5d974
Kevin Ryan <kevinryan1973@gmail.com>
2024-06-23 20:46:06 +0000

    wip

---
+++ components/pages/actions.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export function Actions() {
+
+    return (
+
+        <section>
+        <div className="pt-36" style={{ backgroundColor: '#1e2129' }}>
+            <div className="mx-auto px-6 max-w-6xl text-gray-500">
+                <div className="text-center">
+                    <h2 className="text-3xl text-gray-950 dark:text-white font-semibold">GitHub Actions - Open-Source Pro Support</h2>
+                    <p className="text-sm mt-6 text-gray-700 dark:text-gray-300">Open-source GitHub Actions with the additional confidence of Repo Racers Pro Support.</p>
+                </div>
+                <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                    <div className="relative group overflow-hidden p-[--card-padding] rounded-[--card-border-radius] bg-white border border-[--ui-light-border-color] dark:border-[--ui-dark-border-color] dark:bg-[--card-dark-bg]">
+                        <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-warning-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-0 dark:group-hover:opacity-5"></div>
+                        <div className="relative">
+                            <div className="border border-warning-500/10 flex relative *:relative *:size-6 *:m-auto size-12 rounded-[calc(var(--card-border-radius)/2)] dark:bg-gray-900 dark:border-white/15 before:rounded-[calc((var(--card-border-radius)/2)-1px)] before:absolute before:inset-0 before:border-t before:border-white before:from-warning-100 dark:before:border-white/20 before:bg-gradient-to-b dark:before:from-white/10 dark:before:to-transparent before:shadow dark:before:shadow-gray-950">
+                                <svg className="text-[#000014] dark:text-white" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 150 150">
+                                     <path id="svg_1" fill="none" d="m0,0l150,0l0,150l-150,0l0,-150z" className="cls-2"/>
+                                     <path id="svg_2" fill="#05bdba" d="m43.91,116.64l-1.34,0l-6.67,-6.67l0,-1.34l10.19,-10.19l7.06,0l0.94,0.94l0,7.06l-10.19,10.19l0.01,0.01z" className="cls-3"/>
+                                     <path id="svg_3" fill="#05bdba" d="m35.9,41.22l0,-1.34l6.67,-6.67l1.34,0l10.19,10.19l0,7.06l-0.94,0.94l-7.06,0l-10.19,-10.19l-0.01,0.01z" className="cls-3"/>
+                                     <path id="svg_4" fill="#014847" d="m94.6,95.14l-9.7,0l-0.81,-0.81l0,-22.71c0,-4.04 -1.59,-7.17 -6.46,-7.28c-2.51,-0.07 -5.38,0 -8.44,0.12l-0.46,0.47l0,29.39l-0.81,0.81l-9.7,0l-0.81,-0.81l0,-38.79l0.81,-0.81l21.83,0c8.48,0 15.36,6.88 15.36,15.36l0,24.25l-0.81,0.81z" className="cls-1"/>
+                                     <path id="svg_5" fill="#05bdba" d="m45.29,80.6l-38.8,0l-0.81,-0.81l0,-9.72l0.81,-0.81l38.8,0l0.81,0.81l0,9.72l-0.81,0.81z" className="cls-3"/>
+                                     <path id="svg_6" fill="#05bdba" d="m146.34,80.6l-38.8,0l-0.81,-0.81l0,-9.72l0.81,-0.81l38.8,0l0.81,0.81l0,9.72l-0.81,0.81z" className="cls-3"/>
+                                     <path id="svg_7" fill="#05bdba" d="m70.82,42.6l0,-29.1l0.81,-0.81l9.72,0l0.81,0.81l0,29.1l-0.81,0.81l-9.72,0l-0.81,-0.81z" className="cls-3"/>
+                                     <path id="svg_8" fill="#05bdba" d="m70.82,136.36l0,-29.1l0.81,-0.81l9.72,0l0.81,0.81l0,29.1l-0.81,0.81l-9.72,0l-0.81,-0.81z" className="cls-3"/>
+                                   </svg>
+                            </div>
+
+                            <div className="mt-6 pb-6 rounded-b-[--card-border-radius]">
+                                <p className="text-xs text-gray-700 dark:text-gray-300">This GitHub Action checks that the PR title adheres to the <a href="https://www.conventionalcommits.org/en/v1.0.0/" target="_blank">Conventional Commits</a> specification. If the PR title contains a valid task type and optionally a task number, it labels the PR based on the task type.<br/><br/></p>
+                            </div>
+                            <div className="flex gap-3 -mb-[--card-padding] py-4 border-t border-[--ui-light-border-color] dark:border-[--ui-dark-border-color]">
+                                <a href="https://github.com/marketplace/actions/conventional-commit-in-pull-requests-repo-racers-pro-support" target="_blank" className="group rounded-[--btn-border-radius] disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-gray-950 bg-gray-100 hover:bg-gray-200/75 active:bg-gray-100 dark:text-white dark:bg-gray-500/10 dark:hover:bg-gray-500/15 dark:active:bg-gray-500/10 flex gap-1.5 items-center text-sm h-8 px-3.5 justify-center">
+                                    <span>Marketplace</span>
+                                </a>
+                                <a href="https://github.com/repo-racers/pr-conventional-commits" target="_blank" className="group flex items-center rounded-[--btn-border-radius] disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-gray-950 bg-gray-100 hover:bg-gray-200/75 active:bg-gray-100 dark:text-white dark:bg-gray-500/10 dark:hover:bg-gray-500/15 dark:active:bg-gray-500/10 size-8 justify-center">
+                                    <span className="sr-only">Source Code</span>
+                                    <svg className="size-5" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/></svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    );
+}
