2eaa4d3e14954b8b326c95d5abacb1116454a9f3
Kevin Ryan <kevinryan1973@gmail.com>
2024-06-23 08:58:06 +0000

    fix compile errors in hero component

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -38,8 +38,8 @@ export function Hero() {
                             Introducing Elite Membership !
                             <div className="flex items-center -space-x-3 group-hover:-translate-x-1 transition-transform duration-300">
                                 <span className="w-2.5 translate-y-[-0.3px] -translate-x-px opacity-0 h-[1.5px] rounded bg-gray-950 dark:bg-white origin-left scale-x-0 transition duration-300 group-hover:opacity-100 group-hover:scale-x-100"></span>
-                                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" className="size-4 text-gray-950 dark:text-white -translate-x-2 transition duration-300 group-hover:translate-x-px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
+                                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" className="size-4 text-gray-950 dark:text-white -translate-x-2 transition duration-300 group-hover:translate-x-px" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7"></path>
                                 </svg>
                             </div>
                         </span>
